33098a559bd935b30b3ca4031269aa98b017a1bc
fjg2016 <703911220@qq.com>
2016-11-14 22:02:27 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/res/drawable/shape.xml
    #	app/src/main/res/layout/search_layout.xml
    fjg_5.1.2

--- app/src/main/java/com/jju/edu/aiqiyi/HuiYuanActivity.java
+++ app/src/main/java/com/jju/edu/aiqiyi/HuiYuanActivity.java
@@ -7,8 +7,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.jju.edu.aiqiyi.vipuser.MyVipFragment;
 import com.jju.edu.aiqiyi.vipuser.VipHomeFragment;
