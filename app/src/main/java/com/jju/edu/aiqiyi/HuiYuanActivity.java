af2bfb553f23b6d403ee2fb8fe502aae2e51ff96
LHbuddy <971935542@qq.com>
2016-11-14 22:09:59 +0800

    Merge remote-tracking branch 'github/master'
    # Conflicts:
    #	app/src/main/java/com/jju/edu/aiqiyi/HuiYuanActivity.java
    1

--- app/src/main/java/com/jju/edu/aiqiyi/HuiYuanActivity.java
+++ app/src/main/java/com/jju/edu/aiqiyi/HuiYuanActivity.java
@@ -1,22 +1,18 @@
 package com.jju.edu.aiqiyi;
 
-<<<<<<< HEAD
 import android.graphics.drawable.ColorDrawable;
-=======
 import android.graphics.Color;
->>>>>>> github/master
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-<<<<<<< HEAD
 import android.widget.PopupWindow;
 import android.widget.Toast;
-=======
 
 import com.jju.edu.aiqiyi.vipuser.MyVipFragment;
 import com.jju.edu.aiqiyi.vipuser.VipHomeFragment;
@@ -25,17 +21,11 @@ import com.jju.edu.aiqiyi.vipuser.VipRecordFragment;
 
 import java.util.ArrayList;
 import java.util.List;
->>>>>>> github/master
 
 /**
  * Created by 凌浩 on 2016/11/14.
  */
 
-<<<<<<< HEAD
-public class HuiYuanActivity extends BaseActivity{
-    private ImageView search,plus;
-    private PopupWindow popupWindow;
-=======
 public class HuiYuanActivity extends BaseFragmentActivity{
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -46,14 +36,20 @@ public class HuiYuanActivity extends BaseFragmentActivity{
     private Fragment viphome_fragment;
     private Fragment vipmovie_fragment;
     private Fragment viprecord_fragment;
+    private ImageView search,plus;
+    private PopupWindow popupWindow;
 
     private static final String[] vip_titles = new String[]{"我的VIP会员","会员俱乐部","VIP会员","VIP纪录片"};
 
->>>>>>> github/master
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tuijian_layout);
+        setContentView(R.layout.huiyuan_layout);
+        search = (ImageView) findViewById(R.id.search);
+        plus = (ImageView) findViewById(R.id.plus);
+        //开始监听
+        search.setOnClickListener(new myclick());
+        plus.setOnClickListener(new myclick());
 
         initView();
     }
@@ -80,21 +76,16 @@ public class HuiYuanActivity extends BaseFragmentActivity{
         tabLayout.setTabTextColors(Color.BLACK,Color.argb(255,16,225,37));
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
 
+
     }
 
-
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    class myclick implements View.OnClickListener{
 
         @Override
-<<<<<<< HEAD
         public void onClick(View v) {
-            switch (v.getId()){
+            switch (v.getId()) {
                 case R.id.search:
-                    Toast.makeText(HuiYuanActivity.this,"",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(HuiYuanActivity.this, "", Toast.LENGTH_SHORT).show();
                     break;
                 case R.id.plus:
                     View view = getLayoutInflater().inflate(R.layout.popupwindow_layout, null);
@@ -107,15 +98,26 @@ public class HuiYuanActivity extends BaseFragmentActivity{
                     //设置出现在当前点击控件的正下方
                     popupWindow.showAsDropDown(plus);
                     break;
-=======
+            }
+        }
+    }
+
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
         public Fragment getItem(int position) {
             return vip_fragments.get(position);
         }
->>>>>>> github/master
 
         @Override
         public int getCount() {
-            return vip_titles.length;
+            return vip_fragments.size();
         }
 
         @Override
