331a43033ef17ee867d15424b85da2179f368d6f
Mr feng <fenglv220@gmail.com>
2016-11-16 10:42:59 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/jju/edu/aiqiyi/DaoHangActivity.java
    #	app/src/main/res/layout/daohang_layout.xml
    fyg 1.0.1

--- app/src/main/java/com/jju/edu/aiqiyi/tuijian/DianShiJuFragment.java
+++ app/src/main/java/com/jju/edu/aiqiyi/tuijian/DianShiJuFragment.java
@@ -29,6 +29,7 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.ImageScaleType;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.display.RoundedBitmapDisplayer;
+import com.squareup.picasso.Picasso;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -68,13 +69,13 @@ public class DianShiJuFragment extends Fragment {
 
                 try {
                     Document doc = Jsoup.connect("http://tv.sohu.com/drama/").get();
-                    Elements elements = doc.select("div.colL");
+                    Elements elements = doc.select("div.colR");
                     Element element = elements.get(0);
-                    String name = element.getElementsByTag("span").text();
+                    String name = element.getElementsByTag("a").get(1).text();
                     String image = element.getElementsByTag("img").first().attr("src");
                     //  image.substring();
                     Log.i("TAG", image.toString());
-                    VideoUtil util = new VideoUtil(name, image, "","");
+                    VideoUtil util = new VideoUtil("", image, name, "");
                     olist.add(util);
                     handler.sendEmptyMessage(123);
 
@@ -92,8 +93,8 @@ public class DianShiJuFragment extends Fragment {
             super.handleMessage(msg);
             if (msg.what == 123) {
                 Log.i("TAG", olist.get(0).getVideo_image());
-                ImageLoader.getInstance().displayImage(olist.get(0).getVideo_image(), img_dianshiju, options);
                 tv_dianshiju.setText(olist.get(0).getVideo_name());
+                Picasso.with(getContext()).load(olist.get(0).getVideo_image()).placeholder(R.drawable.book_card_icon).resize(700, 400).into(img_dianshiju);
             }
         }
     };
@@ -102,21 +103,6 @@ public class DianShiJuFragment extends Fragment {
         tv_dianshiju = (TextView) view.findViewById(R.id.tv_dianshiju);
         img_dianshiju = (ImageView) view.findViewById(R.id.img_dianshiju);
 
-        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(getActivity())
-                .denyCacheImageMultipleSizesInMemory()
-                .diskCacheFileCount(100)
-                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
-                .diskCacheSize(100 * 1024 * 1024)
-                .tasksProcessingOrder(QueueProcessingType.LIFO).build();
-        ImageLoader.getInstance().init(configuration);
-
-        options = new DisplayImageOptions.Builder().cacheInMemory(true)
-                .bitmapConfig(Bitmap.Config.ARGB_8888)
-                .imageScaleType(ImageScaleType.EXACTLY)
-                .showImageOnLoading(R.mipmap.ic_launcher)
-                .showImageOnFail(R.mipmap.ic_launcher)
-                .showImageForEmptyUri(R.mipmap.ic_launcher)
-                .displayer(new RoundedBitmapDisplayer(20)).build();
 
     }
 
